c1d7c0905bce8d7ee9f4a2acbdf7b5180e3d33a0
Maureen Josephine <m.akelo@carepay.com>
2020-04-03 17:55:17 +0300

    create simple helloworld method

--- src/main/java/com/example/dependencyinjectiondemo/contollers/MyController.java
+++ src/main/java/com/example/dependencyinjectiondemo/contollers/MyController.java
@@ -1,4 +1,13 @@
 package com.example.dependencyinjectiondemo.contollers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+
+    public String sayHello(){
+        System.out.println("Heey there");
+
+        return "Hello folks";
+    }
 }
