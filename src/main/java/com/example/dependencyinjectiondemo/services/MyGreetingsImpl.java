5d5f87a7bd784fee6d0862af2d6d5faf6a01bedf
Maureen Josephine <m.akelo@carepay.com>
2020-04-03 18:56:37 +0300

    create service impl

---
+++ src/main/java/com/example/dependencyinjectiondemo/services/MyGreetingsImpl.java
@@ -0,0 +1,8 @@
+package com.example.dependencyinjectiondemo.services;
+
+public class MyGreetingsImpl implements MyGreetings {
+    @Override
+    public String sayGreeting() {
+        return null;
+    }
+}
