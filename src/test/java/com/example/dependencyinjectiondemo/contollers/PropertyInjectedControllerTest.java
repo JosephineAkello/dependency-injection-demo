c21123f7ba63a48be4aab20a1ab945cdc76830e8
Maureen Josephine <m.akelo@carepay.com>
2020-04-03 19:27:29 +0300

    place test cases

--- src/test/java/com/example/dependencyinjectiondemo/contollers/PropertyInjectedControllerTest.java
+++ src/test/java/com/example/dependencyinjectiondemo/contollers/PropertyInjectedControllerTest.java
@@ -1,9 +1,27 @@
 package com.example.dependencyinjectiondemo.contollers;
 
+import com.example.dependencyinjectiondemo.services.MyGreetingsImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjectedControllerTest {
 
+    PropertyInjectedController propertyInjectedController;
 
+    @BeforeEach
+    void SetUp(){
+        propertyInjectedController = new PropertyInjectedController();
+        propertyInjectedController.myGreetings = new MyGreetingsImpl();
+    }
+
+
+    @Test
+    void getGreeting(){
+
+        System.out.println(propertyInjectedController.getGreeting());
+
+}
 
 }
