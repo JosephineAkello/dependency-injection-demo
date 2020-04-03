ab2828cbbf936a0c9bc17a98a3fdb77f6299110e
Maureen Josephine <m.akelo@carepay.com>
2020-04-03 19:17:53 +0300

    return string

--- src/main/java/com/example/dependencyinjectiondemo/services/MyGreetingsImpl.java
+++ src/main/java/com/example/dependencyinjectiondemo/services/MyGreetingsImpl.java
@@ -3,6 +3,6 @@ package com.example.dependencyinjectiondemo.services;
 public class MyGreetingsImpl implements MyGreetings {
     @Override
     public String sayGreeting() {
-        return null;
+        return "Hello world";
     }
 }
