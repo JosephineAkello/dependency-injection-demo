3ed33e58b62e441f2ee0fb5bcd2515065b208984
Maureen Josephine <m.akelo@carepay.com>
2020-04-03 17:56:16 +0300

    get bean from the application context

--- src/main/java/com/example/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
+++ src/main/java/com/example/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.dependencyinjectiondemo;
 
+import com.example.dependencyinjectiondemo.contollers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeeting = myController.sayHello();
+
+		System.out.println(greeeting);
 	}
 
 }
