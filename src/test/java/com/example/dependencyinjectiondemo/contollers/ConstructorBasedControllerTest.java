4d5577dda6331a53658cae50eddc90f1a2a162e6
Maureen Josephine <m.akelo@carepay.com>
2020-04-04 13:47:50 +0300

    create constructorbased class

---
+++ src/test/java/com/example/dependencyinjectiondemo/contollers/ConstructorBasedControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.dependencyinjectiondemo.contollers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstructorBasedControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getGreeting() {
+    }
+}
