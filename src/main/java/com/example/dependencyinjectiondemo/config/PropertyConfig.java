f499705331cb4fba58235f28d9f5547ced1492b4
Maureen Josephine <m.akelo@carepay.com>
2020-05-18 22:29:17 +0300

    create datasource properties

---
+++ src/main/java/com/example/dependencyinjectiondemo/config/PropertyConfig.java
@@ -0,0 +1,36 @@
+package com.example.dependencyinjectiondemo.config;
+
+import com.example.dependencyinjectiondemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+public class PropertyConfig {
+
+    @Value("{username}")
+    String user;
+
+    @Value("{password}")
+    String password;
+
+    @Value("url")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+
+        return fakeDataSource();
+    }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
